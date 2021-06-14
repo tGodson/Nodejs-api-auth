c3e62dc9f8f44a3fa07e459a5e11a980cd883bd6
tGodson <tendongze95@gmail.com>
2021-06-14 16:58:21 +0100

    add routes and middlewares to index.js file

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 const express = require('express');
 const app = express();
 
+//import routes
+const authRoute = require('./routes/auth');
+
+//import middlewares
+app.use('/api/user', authRoute);
+
 app.listen(3000, () => console.log('server is up and running'));
