e3a65af5c2a0c6e6a8baf6439aab0871a815c013
tGodson <tendongze95@gmail.com>
2021-06-16 11:07:39 +0100

    connect application to dmongodb database and create a .env file

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+//connect to db
+mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true }, () => 
+    console.log('connected to db')
+);
 
 //import routes
 const authRoute = require('./routes/auth');
